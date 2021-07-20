67d4218679a9709edba51fb8a3319b0422075a36
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2021-07-20 20:59:04 +0530

    Fix the newsletter submit form

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -35,22 +35,65 @@ export default function Subscribe() {
   };
 
   return (
-    <form onSubmit={subscribe}>
-      <label htmlFor="email-input">{'Email Address'}</label>
-      <input
-        id="email-input"
-        name="email"
-        placeholder="you@awesome.com"
-        ref={inputEl}
-        required
-        type="email"
-      />
-      <div>
-        {message
-          ? message
-          : `I'll only send emails when new content is posted. No spam.`}
+    <div className="border border-blue-200 rounded p-6 my-4 w-full dark:border-blue-800 bg-blue-50 dark:bg-blue-900">
+      <h5 className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
+        Subscribe to the newsletter
+      </h5>
+      <p className="my-1 text-gray-800 dark:text-gray-200">
+        Get emails from me about machine learning, tech, statups and more.
+      </p>
+      <div className="relative my-4">
+        <input
+          ref={inputEl}
+          aria-label="Email for newsletter"
+          placeholder="tim@apple.com"
+          type="email"
+          autoComplete="email"
+          required
+          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+        />
+        <button
+          className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
+          onClick={subscribe}
+        >
+          {form.state === 'loading' ? (
+            <svg
+              className="animate-spin h-5 w-5 text-gray-900 dark:text-gray-100"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />
+            </svg>
+          ) : (
+            'Subscribe'
+          )}
+        </button>
       </div>
-      <button type="submit">{'✨ Subscribe 💌'}</button>
-    </form>
+      {form.state === 'error' ? (
+        <ErrorMessage>{form.message}</ErrorMessage>
+      ) : form.state === 'success' ? (
+        <SuccessMessage>{form.message}</SuccessMessage>
+      ) : (
+        <p className="text-sm text-gray-800 dark:text-gray-200">
+          {`${subscriberCount || '-'} subscribers  `}
+          {/*<Link href="/newsletter">*/}
+          {/*  <a>26 issues</a>*/}
+          {/*</Link>*/}
+        </p>
+      )}
+    </div>
   );
 }
